31b3402c255b5e7900e904ea44c1e47f165393b0
Ken Ming Chang <kchang@alphacrc.world>
2024-01-03 09:56:21 +0000

    Updated footer to auto get the current year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
+
+    let currentYear = new Date().getFullYear();
+
     return(
         <footer>
             <div className="text-center my-3">
                 <h6 className="text-secondary">
-                    <span>&copy; 2023 </span><span>Alpha </span><span>CRC</span><span>AI power within</span>
+                    <span>&copy; {currentYear} </span><span>Alpha </span><span>CRC</span><span>AI power within</span>
                 </h6>
             </div>
         </footer>
